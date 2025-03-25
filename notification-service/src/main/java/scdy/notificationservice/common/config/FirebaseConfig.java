b915d294bef79863a42cb3dfa7f37f0df100d633
Yerim <yearm404@naver.com>
2025-03-25 11:49:01 +0900

    fix: FCM key 환경변수 등록, 코드 수정 (#10)

--- notification-service/src/main/java/scdy/notificationservice/common/config/FirebaseConfig.java
+++ notification-service/src/main/java/scdy/notificationservice/common/config/FirebaseConfig.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +18,9 @@ public class FirebaseConfig {
     @Bean
     public FirebaseMessaging firebaseMessaging(){
         try{
-            FileInputStream serviceAccount = new FileInputStream("C:/Users/Yearm404/Desktop/project/Triplan/triplan_backend/serviceAccountKey.json");
+            String FcmServiceAccount = System.getenv("FCM_SECRET_KEY");
+            FileInputStream serviceAccount = new FileInputStream(FcmServiceAccount);
+
             FirebaseOptions firebaseOptions = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
@@ -29,9 +30,7 @@ public class FirebaseConfig {
             //e.printStackTrace();
             log.error("error", e);
         }
-
         return FirebaseMessaging.getInstance();
     }
-
 }
 
